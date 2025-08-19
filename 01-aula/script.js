0f23e9830febd951e60b64539ca732514d75312f
gabrielafortunatokeller-del <gabriela.keller@aluno.senai.br>
2025-08-19 16:49:44 -0300

    iniciando javscript

---
+++ 01-aula/script.js
@@ -0,0 +1,108 @@
+//comentario muito comentante
+
+/*
+comenta 
+varias 
+vezes
+*/
+console.log("uma mensagem muito legal titio greg")
+
+
+ //alert ("testa ai e vê")
+ //cria uma janela de alerta na pagina 
+
+ //VAR cria uma variavel na tela
+ var nome = "gabriela"
+
+ console.log("o nome é:" + nome)
+
+ //cadeia = String
+ var texto = "Santos perdeu de 6x0 para o vasco"
+ console.log (typeof (texto))
+
+//inteiro=number, int
+ var numero = 1990
+  console.log (typeof (numero))
+
+// real =  number, float
+   var quebrado = 1.4
+  console.log (typeof (quebrado))
+
+//logico=bool
+ var VouF = true
+  console.log (typeof (VouF))
+
+  //valor nulo
+ var nulo = null
+  console.log (typeof (nulo))
+
+  //Variavel sem valor iniciado
+ var indefinido = undefined
+ console.log (typeof (indefinido))
+
+ // operadores aritimeticos
+ // *,-,+,/,%
+ var a = 10
+ var b = 5
+ var soma = a + b
+ console.log("A some é:" + soma)
+ var a = 10
+ var b = 5
+  var subtracao = a - b
+ console.log("A subtracao é:" + subtracao)
+ var a = 10
+ var b = 5
+  var multiplicacao= a * b
+ console.log("A multiplicacao é:" +multiplicacao)
+ var a = 10
+ var b = 5
+  var divisao = a / b
+ console.log("A divisao é:" + divisao)
+  var a = 10
+ var b = 5
+  var indefinido  = a % b
+ console.log("A indefinido  é:" + indefinido)
+
+ //operadores logicos
+ //&& (e), || (ou), ! (nao)
+
+ var verdade = true 
+ var mentira =  false
+
+//comparação com E
+ console.log(verdade&& mentira )
+ //comparação com OU
+console.log(verdade || mentira )
+//comparação com negação
+console.log(verdade&& mentira )
+
+
+//operadores relacionais
+// >,<,>=,<=,==,===,!=, !==
+
+var v1 = 7
+var v2 = 15
+var v3 = "7"
+
+console.log (v1>v2)
+console.log (v1<v2)
+console.log (v1>=v2)
+console.log (v1<=v2)
+console.log (v1==v2)
+console.log (v1!=v2)
+
+console.log (v1==v3) //compara só valor
+console.log (v1===v3) //compara valor e tipo
+console.log (v1!==v3) //compara valor e tipo
+
+ var numero = prompt ("Digite um numero:")
+  var numero2 = prompt ("Digite outro numero:")
+
+  var resultado = Number (numero) +  Number(numero2)
+
+  console.log ( "o resultado é:",resultado)
+
+  alert ("o resultado é:" + resultado)
+
+
+
